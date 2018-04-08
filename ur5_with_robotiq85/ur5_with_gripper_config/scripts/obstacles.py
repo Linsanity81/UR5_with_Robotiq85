876fb03540a3f77e1a78f8359747e8af1424b90c
Rulin Chen <37648478+Linsanity81@users.noreply.github.com>
2018-04-08 14:29:01 +0800

    Update obstacles.py

--- ur5_with_robotiq85/ur5_with_gripper_config/scripts/obstacles.py
+++ ur5_with_robotiq85/ur5_with_gripper_config/scripts/obstacles.py
@@ -1,25 +1,10 @@
 #!/usr/bin/env python
 
 """
-    moveit_pick_and_place_demo.py - Version 0.1 2014-01-14
-    
-    Command the gripper to grasp a target object and move it to a new location, all
-    while avoiding simulated obstacles.
-    
-    Created for the Pi Robot Project: http://www.pirobot.org
-    Copyright (c) 2014 Patrick Goebel.  All rights reserved.
+    obstacles.py - Version 0.1 2018.4.2
+    This file is created for UR5 with robotiq85 project.
+    The maintainer is Rulin Chen(Linsanity81) from Shantou University in China.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.5
-    
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details at:
-    
-    http://www.gnu.org/licenses/gpl.html
 """
 
 import rospy, sys
